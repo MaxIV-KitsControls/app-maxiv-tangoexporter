4ca1d0529bf7c6d4bc7c9a05905db135b69dc43d
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2016-11-23 12:01:25 +0100

    v0.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-tangoexporter",
-    version="0.0.5",
+    version="0.0.6",
     description="Prometheus exporter for TANGO device servers.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
